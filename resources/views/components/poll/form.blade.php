51e517734a64e5f53dba6c05cd52a59f2c2101f7
Abdurrahman Shofy Adianto <azophy@gmail.com>
2021-12-18 15:05:22 +0700

    Refactor main voting form into separate component

---
+++ resources/views/components/poll/form.blade.php
@@ -0,0 +1,41 @@
+@props(['pollname' => 'default'])
+
+<form>
+      <div style="background:#ddd; padding:0 1em">
+          <label for="budget">Your Voting Budget
+            <progress id="{{$pollname}}-budget" value="100" max="100"></progress>
+          </label>
+
+          <div id="{{$pollname}}-notif"></div>
+      </div>
+
+      @foreach (['Mountain', 'Beach', 'Farm', 'Museum'] as $item)
+      <label for="choice-{{$loop->index}}">{{$item}}
+        <input type="range" min="0" max="10" value="0" id="choice-{{$loop->index}}" name="choice-{{$loop->index}}" class="{{$pollname}}-item" oninput="updateBudget('{{$pollname}}')">
+      </label>
+      @endforeach
+
+      <button id="{{$pollname}}-submit" type="submit">Submit</button>
+</form>
+
+<script>
+function updateBudget(classname) {
+    var total = 0
+    document.querySelectorAll('.' + classname + '-item').forEach(i => total += Number(i.value) * Number(i.value) )
+    console.log(total)
+    document.getElementById(classname + '-budget').value = 100 - total
+
+    var notif = document.getElementById(classname + '-notif')
+    var submit = document.getElementById(classname + '-submit')
+    if (total > 100) {
+        notif.innerText = 'You has exceeded your budget! Adjust you choices so they still within range'
+        submit.disabled = true
+    } else {
+        notif.innerText = ''
+        submit.disabled = false
+    }
+}
+
+updateBudget('{{$pollname}}')
+</script>
+
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -37,50 +37,14 @@
       <section id="example">
         <h2>Example: where should we go for this holliday?</h2>
         <p>Sed ultricies dolor non ante vulputate hendrerit. Vivamus sit amet suscipit sapien. Nulla iaculis eros a elit pharetra egestas.</p>
-        <form>
-          <div style="background:#ddd; padding:0 1em">
-              <label for="budget">Your Voting Budget
-                <progress id="myvote-budget" value="100" max="100"></progress>
-              </label>
 
-              <div id="myvote-notif"></div>
-          </div>
-
-          @foreach (['Mountain', 'Beach', 'Farm', 'Museum'] as $item)
-          <label for="choice-{{$loop->index}}">{{$item}}
-            <input type="range" min="0" max="10" value="0" id="choice-{{$loop->index}}" name="choice-{{$loop->index}}" class="myvote-item" oninput="updateBudget('myvote')">
-          </label>
-          @endforeach
-
-          <button id="myvote-submit" type="submit">Submit</button>
-        </form>
-
-        <script>
-        function updateBudget(classname) {
-            var total = 0
-            document.querySelectorAll('.' + classname + '-item').forEach(i => total += Number(i.value) * Number(i.value) )
-            console.log(total)
-            document.getElementById(classname + '-budget').value = 100 - total
-
-            var notif = document.getElementById(classname + '-notif')
-            var submit = document.getElementById(classname + '-submit')
-            if (total > 100) {
-                notif.innerText = 'You has exceeded your budget! Adjust you choices so they still within range'
-                submit.disabled = true
-            } else {
-                notif.innerText = ''
-                submit.disabled = false
-            }
-        }
-
-        updateBudget('myvote')
-        </script>
+        <x-poll.form/>
 
     </main><!-- ./ Main -->
 
     <!-- Footer -->
     <footer>
-      <small>Created by <a href="https://abdurrahman.adianto.id">Abdurrahman Shofy Adianto</a> • Built with <a href="laravel.com">Laravel</a> and <a href="https://picocss.com">PicoCSS</a>  </small>
+      <small>Created by <a target="_blank" href="https://abdurrahman.adianto.id">Abdurrahman Shofy Adianto</a> • Built with <a target="_blank" href="https://laravel.com">Laravel</a> and <a target="_blank" href="https://picocss.com">PicoCSS</a>  </small>
     </footer><!-- ./ Footer -->
 
   </body>
